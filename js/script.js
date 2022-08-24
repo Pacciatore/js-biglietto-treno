a209ebcfdff4ffb49617dc87b289db5f499c5469
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-08-24 09:44:46 +0200

    Aggiunta possibilità di acquistare più biglietti

--- js/script.js
+++ js/script.js
@@ -11,50 +11,57 @@ let etaPasseggero;
 
 let prezzoBiglietto;
 
+let fineAcquisto;
+
 // Inizio istruzioni
 
-
-// Richiesta km da percorrere con controllo del valore numerico
 do {
-    kmDaPercorrere = parseFloat(prompt('Inserire numero di kilometri che si vuole percorrere:'));
-    console.log('kmDaPercorrere ' + kmDaPercorrere);
 
-    // Messaggio di errore
-    if (isNaN(kmDaPercorrere)) {
-        alert('Il valore inserito non è un valore numerico.');
-    }
+    // Richiesta km da percorrere con controllo del valore numerico
+    do {
+        kmDaPercorrere = parseFloat(prompt('Inserire numero di kilometri che si vuole percorrere:'));
+        console.log('kmDaPercorrere ' + kmDaPercorrere);
 
-} while (isNaN(kmDaPercorrere));
+        // Messaggio di errore
+        if (isNaN(kmDaPercorrere)) {
+            alert('Il valore inserito non è un valore numerico.');
+        }
 
-// Richiesta età del passegero con controllo del valore numerico
-do {
-    etaPasseggero = parseFloat(prompt('Inserire età passeggero:'));
-    console.log('etaPassegero ' + etaPasseggero);
+    } while (isNaN(kmDaPercorrere));
 
-    // Messaggio di errore
-    if (isNaN(etaPasseggero)) {
-        alert('Il valore inserito non è un valore numerico.');
-    }
+    // Richiesta età del passegero con controllo del valore numerico
+    do {
+        etaPasseggero = parseFloat(prompt('Inserire età passeggero:'));
+        console.log('etaPassegero ' + etaPasseggero);
 
-} while (isNaN(etaPasseggero));
+        // Messaggio di errore
+        if (isNaN(etaPasseggero)) {
+            alert('Il valore inserito non è un valore numerico.');
+        }
 
-// Calcolo del costo del biglietto
-prezzoBiglietto = costoPerKm * kmDaPercorrere;
-console.log('prezzoBiglietto ' + prezzoBiglietto);
+    } while (isNaN(etaPasseggero));
+
+    // Calcolo del costo del biglietto
+    prezzoBiglietto = costoPerKm * kmDaPercorrere;
+    console.log('prezzoBiglietto ' + prezzoBiglietto);
 
 
-// Se il passeggero è maggiorenne controlli se è senior ed applica gli sconti di conseguenza,
-// Altrimenti stampi il prezzo del biglietto pieno
-if (etaPasseggero >= maggioreEta) {
+    // Se il passeggero è maggiorenne controlli se è senior ed applica gli sconti di conseguenza,
+    // Altrimenti stampi il prezzo del biglietto pieno
+    if (etaPasseggero >= maggioreEta) {
 
-    if (etaPasseggero >= seniorEta) {
-        prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * scontoSenior);
-        alert('Il prezzo del biglietto è ' + prezzoBiglietto.toFixed(2) + '€, sconto per gli over 65 applicato.')
+        if (etaPasseggero >= seniorEta) {
+            prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * scontoSenior);
+            alert('Il prezzo del biglietto è ' + prezzoBiglietto.toFixed(2) + '€, sconto per gli over 65 applicato.')
+        } else {
+            prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * scontoMaggiorenni);
+            alert('Il prezzo del biglietto è ' + prezzoBiglietto.toFixed(2) + '€, sconto per i maggiorenni applicato.')
+        }
+
     } else {
-        prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * scontoMaggiorenni);
-        alert('Il prezzo del biglietto è ' + prezzoBiglietto.toFixed(2) + '€, sconto per i maggiorenni applicato.')
+        alert('Il prezzo del biglietto è ' + prezzoBiglietto.toFixed(2) + '€, senza sconti applicati.')
     }
 
-} else {
-    alert('Il prezzo del biglietto è ' + prezzoBiglietto.toFixed(2) + '€, senza sconti applicati.')
-}
+    fineAcquisto = prompt('Terminare la fase di acquisto? (S/N)');
+
+} while (fineAcquisto === 'N' || fineAcquisto === 'No' || fineAcquisto === 'n' || fineAcquisto === 'no');
