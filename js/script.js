ea99165ca9b235795b94e9313287889b05906c2b
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-08-26 10:45:09 +0200

    Personalizzazione messaggi d'errore

--- js/script.js
+++ js/script.js
@@ -12,9 +12,9 @@ let etaPasseggero;
 let prezzoBiglietto;
 
 let message1 = 'Il prezzo del biglietto è ';
-let message2 = ', senza sconti applicati.';
+let message2 = '€, senza sconti applicati.';
 let message2Min = '€, sconto per i minorenni applicato.';
-let message2Mag = ', sconto per gli over 65 applicato.';
+let message2Mag = '€, sconto per gli over 65 applicato.';
 
 let fineAcquisto;
 
@@ -32,7 +32,11 @@ do {
             alert('Il valore inserito non è un valore numerico.');
         }
 
-    } while (isNaN(kmDaPercorrere));
+        if (kmDaPercorrere <= 0) {
+            alert('Il valore non può essere uguale o inferiore a 0!');
+        }
+
+    } while (isNaN(kmDaPercorrere) || kmDaPercorrere <= 0);
 
     // Richiesta età del passegero con controllo del valore numerico
     do {
@@ -44,7 +48,11 @@ do {
             alert('Il valore inserito non è un valore numerico.');
         }
 
-    } while (isNaN(etaPasseggero));
+        if (etaPasseggero < 0) {
+            alert('Il valore non può essere inferiore a 0!');
+        }
+
+    } while (isNaN(etaPasseggero) || etaPasseggero < 0);
 
     // Calcolo del costo del biglietto
     prezzoBiglietto = costoPerKm * kmDaPercorrere;
